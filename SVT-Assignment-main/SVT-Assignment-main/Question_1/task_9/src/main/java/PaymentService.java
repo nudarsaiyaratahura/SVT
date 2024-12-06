a7b6ad8bb605d77c6221be004d51b9a71291fb39
nudarsaiyaratahura <139535440+nudarsaiyaratahura@users.noreply.github.com>
2024-12-06 22:11:37 +0600

    Delete SVT-Assignment-main/SVT-Assignment-main directory

--- SVT-Assignment-main/SVT-Assignment-main/Question_1/task_9/src/main/java/PaymentService.java
+++
@@ -1,6 +0,0 @@
-public class PaymentService {
-     public void processPayment(double amount) {
-         System.out.println("Processing payment of $" + amount);
-     }
- }
- 
--- SVT-Assignment-main/SVT-Assignment-main/Question_2/task_01/src/main/java/UserService.java
+++
@@ -1,6 +0,0 @@
-// package com.example;
-
-public interface UserService {
-    boolean usernameExists(String username);
-    boolean saveUser(String username, String password);
-}
